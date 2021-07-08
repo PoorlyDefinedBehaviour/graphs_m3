1e67ee810397bc89ec9628f4e7b68859d483f137
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-07-07 23:53:54 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,100 @@
-import "./App.css"
+import { Sigma } from "react-sigma"
+import * as savings from "./Savings"
+
+const randomHexColor = () => `#${Math.random().toString(16).substr(2, 6)}`
 
 function App() {
-  return <h1>hello</h1>
+  const costMatrix = {
+    A: {
+      B: 10,
+      C: 12,
+      D: 18,
+      E: 10,
+      O: 10,
+    },
+    B: {
+      A: 10,
+      C: 7,
+      D: 12,
+      E: 12,
+      O: 5,
+    },
+    C: {
+      A: 12,
+      B: 7,
+      D: 8,
+      E: 12,
+      O: 5,
+    },
+    D: {
+      A: 18,
+      B: 12,
+      C: 8,
+      E: 10,
+      O: 10,
+    },
+    E: {
+      A: 10,
+      B: 12,
+      C: 12,
+      D: 10,
+      O: 10,
+    },
+    O: {
+      A: 10,
+      B: 5,
+      C: 5,
+      D: 10,
+      E: 10,
+    },
+  }
+
+  const routes = savings.calculateRoutes(costMatrix, {
+    startingPoint: "O",
+    dailyVehicleDistance: 30,
+  })
+
+  console.log("aaaa", JSON.stringify(routes, null, 2))
+
+  const nodes = Object.keys(costMatrix).map((vertex, i) => ({
+    id: vertex,
+    label: vertex,
+    size: 100,
+    x: Math.cos(i),
+    y: Math.sin(i),
+  }))
+
+  const edges = routes.flatMap(route => {
+    const color = randomHexColor()
+
+    return route.path.map(edge => ({
+      id: `${edge.from} -> ${edge.to}`,
+      label: `${edge.from} -> ${edge.to}`,
+      source: edge.from,
+      target: edge.to,
+      color,
+    }))
+  })
+  console.log(edges)
+  const graph = {
+    nodes,
+    edges, // : [{ id: "e1", source: "A", target: "B", label: "SEES" }],
+  }
+
+  console.log("graph", JSON.stringify(graph, null, 2))
+
+  return (
+    <Sigma
+      style={{ width: "1000px", height: "1000px" }}
+      graph={graph}
+      settings={{
+        drawLabels: true,
+        drawEdgeLabels: true,
+        drawEdges: true,
+        clone: true,
+      }}
+    />
+  )
 }
 
 export default App
---
+++ src/Savings.js
@@ -0,0 +1,121 @@
+import { uniqBy, equals } from "ramda"
+
+const calculateSavings = (costMatrix, routes, startingPoint) => {
+  const savings = []
+
+  const customers = uniqBy(
+    route => route.customer,
+    routes.flatMap(route =>
+      route.path.map(edge => ({
+        customer: edge.to,
+        routeIndex: edge.routeIndex,
+      }))
+    )
+  )
+
+  for (let i = 0; i < customers.length; i += 1) {
+    for (let j = i + 1; j < customers.length; j += 1) {
+      const from = customers[i]
+      const to = customers[j]
+
+      savings.push({
+        from,
+        to,
+        cost:
+          (costMatrix[from.customer][startingPoint] ?? 0) +
+          (costMatrix[startingPoint][to.customer] ?? 0) -
+          costMatrix[from.customer][to.customer],
+      })
+    }
+  }
+
+  return savings
+}
+
+const buildInitialRoutes = (costMatrix, startingPoint) => {
+  const routes = []
+
+  const customers = Object.keys(costMatrix)
+
+  for (let i = 0; i < customers.length; i += 1) {
+    const customer = customers[i]
+
+    if (customer === startingPoint) {
+      continue
+    }
+
+    const route = {
+      cost:
+        costMatrix[startingPoint][customer] +
+        costMatrix[customer][startingPoint],
+      path: [
+        {
+          from: startingPoint,
+          to: customer,
+          cost: costMatrix[startingPoint][customer],
+          routeIndex: i,
+        },
+        {
+          from: customer,
+          to: startingPoint,
+          cost: costMatrix[customer][startingPoint],
+          routeIndex: i,
+        },
+      ],
+    }
+
+    routes.push(route)
+  }
+
+  return routes
+}
+
+export const calculateRoutes = (
+  costMatrix,
+  { startingPoint, dailyVehicleDistance }
+) => {
+  const routes = buildInitialRoutes(costMatrix, startingPoint)
+  const savings = calculateSavings(costMatrix, routes, startingPoint).sort(
+    (a, b) => a.cost < b.cost
+  )
+
+  const visitedCustomers = new Set()
+
+  for (const { from, to } of savings) {
+    const fromRoute = routes[from.routeIndex]
+    const toRoute = routes[to.routeIndex]
+
+    if (visitedCustomers.has(to.customer) && to.customer !== startingPoint) {
+      continue
+    }
+
+    visitedCustomers.add(to.customer)
+
+    const newConnection = {
+      from: from.customer,
+      to: to.customer,
+      cost: costMatrix[from.customer][to.customer],
+      routeIndex: from.routeIndex,
+    }
+    console.log("newConnection", newConnection)
+    const routePath = [
+      ...fromRoute.path.filter(edge => edge.to !== startingPoint),
+      newConnection,
+      ...toRoute.path.filter(edge => edge.from !== startingPoint),
+    ]
+
+    const routeCost = routePath.reduce((cost, edge) => cost + edge.cost, 0)
+
+    if (routeCost > dailyVehicleDistance || equals(fromRoute, toRoute)) {
+      continue
+    }
+
+    routes[from.routeIndex] = {
+      cost: routeCost,
+      path: routePath,
+    }
+    routes.splice(to.routeIndex, 1)
+  }
+
+  return routes
+}
